95ec2f8b11bb1f0681166e6628c6fdf278d06df5
boujnah <yassineboujnah@gmail.com>
2019-05-04 23:16:19 +0100

    ajouter un fragment

--- app/src/main/java/com/example/iread/controller/EntrainActivty.java
+++ app/src/main/java/com/example/iread/controller/EntrainActivty.java
@@ -1,15 +1,76 @@
 package com.example.iread.controller;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.example.iread.Adapters.BookAdapter;
 import com.example.iread.R;
+import com.example.iread.api.UserHelper;
+import com.example.iread.base.BaseActivity;
+import com.example.iread.model.BookFireBase;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.Query;
 
-public class EntrainActivty extends AppCompatActivity {
+public class EntrainActivty extends BaseActivity {
+
+
+    private CollectionReference bookRef = UserHelper.getUsersCollection().document(getCurrentUser().getUid()).collection("CurrentlyReding");
+
+    private BookAdapter adapter;
+    @Override
+    public int getFragmentLayout() {
+        return R.layout.activity_deja_lu;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_entrain_activty);
+        configureToolbar();
+
+        setUpRecyclerView();
+    }
+
+
+    private void setUpRecyclerView() {
+        Query query = bookRef.orderBy("title");
+        Log.d("dfg",query.toString());
+        FirestoreRecyclerOptions<BookFireBase> options = new FirestoreRecyclerOptions.Builder<BookFireBase>()
+                .setQuery(query, BookFireBase.class)
+                .build();
+        adapter = new BookAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.deja_lu_recycleView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.deja_lu_toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
